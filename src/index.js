ea7a92f82e91fd754929842e67c0d16cd5207c03
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-09-24 19:35:07 -0400

    added redux imports

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import App from './App';
+//!REDUX STUFF 
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from  'redux-thunk'
+import {BrowserRouter as Router} from 'react-router-dom'
+
+//! CUSTOM 
+import './index.css';
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(, composeEnhancer(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <Provider store={null}> */}
+      <Router >
+        <App />
+      </Router>
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
