e34db42b525fafe2c60d9d5ec169ebf7e8915fb2
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-09-25 11:46:37 -0400

    removed loaded emojis action

--- src/Store/emojis.js
+++ src/Store/emojis.js
@@ -1,21 +1,14 @@
 //! REDUCER
 const initialState = {
-        emojis: [],
-        emojisLoading: false
+        emojis: []
 }
 
 export const emojisReducer = (state = initialState, action) => {
         switch(action.type) {
-                case LOAD_EMOJIS_REQUEST: 
-                        return {
-                                ...state, 
-                                emojisLoading: true
-                        }
                 case ADD_LOADED_EMOJIS:
                         return {
                                 ...state,
                                 emojis: action.payload.emojis,
-                                emojisLoading: false
                         }
                 default:
                         return state
@@ -23,16 +16,11 @@ export const emojisReducer = (state = initialState, action) => {
 }
 
 //! TYPES 
-const LOAD_EMOJIS_REQUEST = "LOAD_EMOJIS_REQUEST"
 const ADD_LOADED_EMOJIS = "ADD_LOADED_EMOJIS"
 const SEARCH_FOR_EMOJI = "SEARCH_FOR_EMOJI"
 const FILTER_EMOJIS_BY_CATEGORY = "FILTER_EMOJIS_BY_CATEGORY"
 
 //! CREATORS
-export const loadEmojisRequest = () => ({ //loading emojis begin
-        type: LOAD_EMOJIS_REQUEST
-})
-
 export const addLoadedEmojis = emojis => ({ // adding emojis after loading is finished
         type: ADD_LOADED_EMOJIS,
         payload: {
