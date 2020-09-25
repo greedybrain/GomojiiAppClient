f639cb59d70fb35497ee35470fe04d001a2ec400
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-09-25 10:48:50 -0400

    created HomeContainer

---
+++ src/Pages/HomePage/containers/HomeContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EmojisChildContainer from '../components/EmojisChildContainer'
+
+const HomeContainer = () => {
+        return (
+                <div className="home_page_container">
+                        <EmojisChildContainer />
+                </div>
+        )
+}
+
+export default HomeContainer
