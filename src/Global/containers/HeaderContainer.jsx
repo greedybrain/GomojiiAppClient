b99aa9e697de8527a2b630efebc57aca27fd7fd6
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-09-25 10:47:21 -0400

    created HeaderContainer

---
+++ src/Global/containers/HeaderContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeaderContainer = () => {
+        return (
+                <header>
+                        
+                </header>
+        )
+}
+
+export default HeaderContainer
