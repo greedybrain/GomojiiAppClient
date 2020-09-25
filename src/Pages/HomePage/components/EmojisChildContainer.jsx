e3dc533a1a7cd22fcb2b278a8b2b7648e8bb55e0
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-09-25 10:47:52 -0400

    created EmojisChildContainer

---
+++ src/Pages/HomePage/components/EmojisChildContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EmojisListComponent from './EmojisListComponent'
+
+const EmojisChildContainer = () => {
+        return (
+                <div className="emojis_list_container">
+                        <EmojisListComponent />
+                </div>
+        )
+}
+
+export default EmojisChildContainer
