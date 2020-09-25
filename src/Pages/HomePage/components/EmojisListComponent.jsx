237c1c8d4f3578bb479737dabf3f67d0e9d06861
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-09-25 10:48:37 -0400

    created EmojisListComponent

---
+++ src/Pages/HomePage/components/EmojisListComponent.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { emojiData } from '../../../Data/emojis'
+import { addLoadedEmojis, loadEmojisRequest } from '../../../Store/emojis'
+import ifEmoji from 'if-emoji'
+import EmojiChildComponent from './EmojiChildComponent'
+
+const EmojisListComponent = () => {
+
+        useEffect(() => {
+                loadUpMovies()
+        })
+
+        const dispatch = useDispatch()
+        // const emojisLoading = useSelector(state => state.emojisRed.emojisLoading)
+
+        const loadUpMovies = () => {
+                dispatch(loadEmojisRequest())
+                dispatch(addLoadedEmojis(emojiData))
+        }
+        
+        const renderEmojis = emojiData.map((emoji, index) => {
+                if (!!emoji.variants) {
+                        return (
+                                <li key={index} style={{display: "flex"}}>
+                                        <div><EmojiChildComponent emoji={ emoji } /></div>
+                                        <ul>
+                                                {
+                                                        emoji.variants.map((variant, index) => {
+                                                                return ifEmoji(variant.character) ? <li key={ index }><EmojiChildComponent emoji={variant} /></li> : null
+                                                        })
+                                                }
+                                        </ul>
+                                </li>
+                        )
+                } else {
+                        return ifEmoji(emoji.character) ? <li key={index}>{emoji.character}</li> : null
+                }
+        })
+        return (
+                <ul>
+                        { renderEmojis }
+                </ul>
+        )
+}
+
+export default EmojisListComponent
