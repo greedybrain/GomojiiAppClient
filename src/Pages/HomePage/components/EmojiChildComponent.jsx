4af7e48bf20c26e81bc875e7ac22fda770888b06
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-09-25 10:47:38 -0400

    created EmojiChildComponent

---
+++ src/Pages/HomePage/components/EmojiChildComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EmojiChildComponent = ({ emoji }) => {
+        return (
+                <>{ emoji.character }</>
+        )
+}
+
+export default EmojiChildComponent
