ad049499ae916998789046c46375108e8d5e2db8
greedybrain <greedybrain18@gmail.com>
2020-10-01 16:39:39 -0400

    created signup comp and added signup logic

---
+++ src/Pages/SignupPage/components/SignupComponent.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+const SignupComponent = ({ history }) => {
+        const [email, setEmail] = useState('')
+        const [password, setPassword] = useState('') 
+        const [passwordConfirmation, setPasswordConfirmation] = useState('') 
+
+        const dispatch = useDispatch()
+
+        const user = useSelector(state => ({
+                error: state.authRed.error,
+                loggedIn: state.authRed.loggedIn
+        }))
+
+        const handleChange = event => {
+                if (event.target.name === 'email') {
+                        setEmail(event.target.value)
+                } else if(event.target.name === 'password') {
+                        setPassword(event.target.value)
+                } else {
+                        setPasswordConfirmation(event.target.value)
+                }
+        }
+
+        const handleSubmit = event => {
+                event.preventDefault()
+                history.replace('/')
+        }
+
+        const showErrorMessage = () => {
+                if (user.error) {
+                        return (
+                                <div className="error animate__animated animate__slideInDown animate__faster">
+                                        <p style={{ color: 'red', fontWeight: 'bolder' }}>
+                                                { user.error }
+                                        </p>
+                                </div>
+                        )
+                } else {
+                        return null
+                }
+        }
+
+        return (
+                user.loggedIn 
+                ?
+                (
+                        null
+                )
+                :
+                (
+                        <form onSubmit={handleSubmit}>
+                                { showErrorMessage() }
+                                <div className="email">
+                                        <input 
+                                                type="email" 
+                                                name="email" 
+                                                placeholder="Email"
+                                                value={email}
+                                                onChange={handleChange}
+                                        />
+                                </div>
+                                <div className="password">
+                                        <input 
+                                                type="password"
+                                                name="password"
+                                                placeholder="Password"
+                                                value={password}
+                                                onChange={handleChange}
+                                        />
+                                </div>
+                                <div className="password_confirmation">
+                                        <input 
+                                                type="password"
+                                                name="password_confirmation"
+                                                placeholder="Confirm Password"
+                                                value={passwordConfirmation}
+                                                onChange={handleChange}
+                                        />
+                                </div>
+                                <div className="submit_btn">
+                                        <button>Login</button>
+                                </div>
+                        </form>
+                )
+                
+        )
+}
+
+export default SignupComponent
