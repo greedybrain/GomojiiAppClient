35fd5131bbd89170fc0120378b14df16ed69f95d
greedybrain <greedybrain18@gmail.com>
2020-10-01 16:40:12 -0400

    ...

--- src/Store/emojis.js
+++ src/Store/emojis.js
@@ -3,7 +3,7 @@ const initialState = {
         emojis: []
 }
 
-export const emojisReducer = (state = initialState, action) => {
+export default function emojisReducer(state = initialState, action) {
         switch(action.type) {
                 case ADD_LOADED_EMOJIS:
                         return {
@@ -17,8 +17,8 @@ export const emojisReducer = (state = initialState, action) => {
 
 //! TYPES 
 const ADD_LOADED_EMOJIS = "ADD_LOADED_EMOJIS"
-const SEARCH_FOR_EMOJI = "SEARCH_FOR_EMOJI"
-const FILTER_EMOJIS_BY_CATEGORY = "FILTER_EMOJIS_BY_CATEGORY"
+// const SEARCH_FOR_EMOJI = "SEARCH_FOR_EMOJI"
+// const FILTER_EMOJIS_BY_CATEGORY = "FILTER_EMOJIS_BY_CATEGORY"
 
 //! CREATORS
 export const addLoadedEmojis = emojis => ({ // adding emojis after loading is finished
