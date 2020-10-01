f27c29088f63214867f8f11b318de9ffa6a4de47
greedybrain <greedybrain18@gmail.com>
2020-10-01 16:38:58 -0400

    ...

--- src/Pages/HomePage/components/EmojisListComponent.jsx
+++ src/Pages/HomePage/components/EmojisListComponent.jsx
@@ -7,27 +7,28 @@ import EmojiChildComponent from './EmojiChildComponent'
 
 const EmojisListComponent = () => {
         
-        const renderEmojis = emojiData.map((emoji, index) => {
-                if (!!emoji.variants) {
-                        return (
-                                <li key={index} style={{display: "flex"}}>
-                                        <div><EmojiChildComponent emoji={ emoji } /></div>
-                                        <ul>
-                                                {
-                                                        emoji.variants.map((variant, index) => {
-                                                                return ifEmoji(variant.character) ? <li key={ index }><EmojiChildComponent emoji={variant} /></li> : null
-                                                        })
-                                                }
-                                        </ul>
-                                </li>
-                        )
-                } else {
-                        return ifEmoji(emoji.character) ? <li key={index}>{emoji.character}</li> : null
-                }
-        })
+        // const renderEmojis = emojiData.map((emoji, index) => {
+        //         if (!!emoji.variants) {
+        //                 return (
+        //                         <li key={index} style={{display: "flex"}}>
+        //                                 <div><EmojiChildComponent emoji={ emoji } /></div>
+        //                                 <ul>
+        //                                         {
+        //                                                 emoji.variants.map((variant, index) => {
+        //                                                         return ifEmoji(variant.character) ? <li key={ index }><EmojiChildComponent emoji={variant} /></li> : null
+        //                                                 })
+        //                                         }
+        //                                 </ul>
+        //                         </li>
+        //                 )
+        //         } else {
+        //                 return ifEmoji(emoji.character) ? <li key={index}>{emoji.character}</li> : null
+        //         }
+        // })
         return (
                 <ul>
-                        { renderEmojis }
+                        { null }
+                        {/* { renderEmojis } */}
                 </ul>
         )
 }
