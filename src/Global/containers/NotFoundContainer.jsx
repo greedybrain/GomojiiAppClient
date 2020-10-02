1793686799802d8a2d0d0779067473463b1afc5d
greedybrain <greedybrain18@gmail.com>
2020-10-02 11:57:32 -0400

    created not found component

---
+++ src/Global/containers/NotFoundContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFoundContainer = () => {
+        return (
+                <div>
+                        <h1>Page Not Found 404</h1>
+                </div>
+        )
+}
+
+export default NotFoundContainer
