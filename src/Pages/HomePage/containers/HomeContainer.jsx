8c99542b576c2673e3e32e91dd911bd82fd9761c
greedybrain <greedybrain18@gmail.com>
2020-10-02 11:58:04 -0400

    added logout component to home container comp

--- src/Pages/HomePage/containers/HomeContainer.jsx
+++ src/Pages/HomePage/containers/HomeContainer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Logout from '../../../Global/components/Logout'
 import EmojisChildContainer from '../components/EmojisChildContainer'
 
 const HomeContainer = () => {
         return (
                 <div className="home_page_container">
+                        <Logout />
                         <EmojisChildContainer />
                 </div>
         )
