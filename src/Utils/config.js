45e10c858b79424dbad984bc42f65931cf6f9301
greedybrain <greedybrain18@gmail.com>
2020-10-04 09:51:36 -0400

    created config to hold endpoint data

---
+++ src/Utils/config.js
@@ -0,0 +1,9 @@
+export const _get = {
+        endpoints: {
+                baseUrl: "http://localhost:3001/api/v1",
+                loginCheck: 'login_check',
+                signup: 'signup',
+                login: 'login',
+                logout: 'logout'
+        }
+}
