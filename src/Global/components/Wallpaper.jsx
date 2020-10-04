2a310ad5e2c289f4020891291a9b6927cd35cc42
greedybrain <greedybrain18@gmail.com>
2020-10-04 09:49:41 -0400

    created wallpaper component

---
+++ src/Global/components/Wallpaper.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Wallpaper = () => {
+        return (
+                <>
+                        <div className="wallpaper"></div>
+                        <div className="wallpaper_overlay">
+                                <div className="image">
+                                        <img src="images/gomojii-logo-white.png" alt="gomojii logo"/>
+                                        <p className="tagline">EVERYTHING EMOJI!</p>
+                                </div>
+                        </div>
+                </>
+        )
+}
+
+export default Wallpaper
