00f78cb3a2ea62ebc249421cd91a2542c92c6960
greedybrain <greedybrain18@gmail.com>
2020-10-04 09:49:00 -0400

    ...

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import HeaderContainer from './Global/containers/HeaderContainer';
+// import HeaderContainer from './Global/containers/HeaderContainer';
 import Routes from './Routes/Routes';
 import { checkLoggedInStatusThunk } from './Store/middleware/authThunk';
 
-function App() {
+function App({ getState }) {
   const dispatch = useDispatch()
-  const user = useSelector(state => ({
-    loggedIn: state.authRed.loggedIn
-  }))
+  const loggedIn = useSelector(state => state.authRed.loggedIn)
+
 
   useEffect(() => {
     dispatch(checkLoggedInStatusThunk())
-  }, [])
+  })
 
   return (
     <div className="App">
-      <HeaderContainer />
+      {/* <HeaderContainer /> */}
       <Routes />
     </div>
   );
