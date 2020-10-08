3474dcc2121268e443008bcb9596e6891915c8e6
greedybrain <greedybrain18@gmail.com>
2020-10-08 08:23:53 -0400

    removed comments

--- src/Store/middleware/emojisThunk.js
+++ src/Store/middleware/emojisThunk.js
@@ -5,23 +5,15 @@ import { filterEmojis, loadEmojis, requestingEmojis } from '../emojis'
 const { baseUrl, emojis } = _get.endpoints
 
 export const loadEmojisThunk = () => {
-        // let cachedEmojis = JSON.parse(localStorage.getItem('emojis'))
         return async dispatch => {
-                // if (cachedEmojis) {
-                //         dispatch(requestingEmojis())
-                //         dispatch(loadEmojis(cachedEmojis))
-                //         return;
-                // } else {
-                        try {
-                                dispatch(requestingEmojis())
-                                const response = await axios(`${baseUrl}/${emojis}`)
-                                const fetchedEmojis = response.data.emojis.data
-                                dispatch(loadEmojis(fetchedEmojis))
-                                dispatch(filterEmojis(fetchedEmojis))
-                                // localStorage.setItem('emojis', JSON.stringify(fetchedEmojis))
-                        } catch(e) {
-                                console.log(e)
-                        }
-                // }
+                try {
+                        dispatch(requestingEmojis())
+                        const response = await axios(`${baseUrl}/${emojis}`)
+                        const fetchedEmojis = response.data.emojis.data
+                        dispatch(loadEmojis(fetchedEmojis))
+                        dispatch(filterEmojis(fetchedEmojis))
+                } catch(e) {
+                        console.log(e)
+                }
         }
 }
