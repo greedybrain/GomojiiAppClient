f557fe3b04763347049c1dc13b6f80532b0e1c80
greedybrain <greedybrain18@gmail.com>
2020-10-08 08:16:32 -0400

    added renderAccEmoji method

--- src/Pages/HomePage/components/EmojisListComponent.jsx
+++ src/Pages/HomePage/components/EmojisListComponent.jsx
@@ -1,35 +1,70 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { emojiData } from '../../../Data/emojis'
-import { addLoadedEmojis, loadEmojisRequest } from '../../../Store/emojis'
+//! BUILT IN OR LIBRARY PACKAGE
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 import ifEmoji from 'if-emoji'
+//! CUSTOM
 import EmojiChildComponent from './EmojiChildComponent'
+import '../../../Assets/emojisContainer.css'
 
 const EmojisListComponent = () => {
+        // const [showVariants, setShowVariants] = useState(false)
+        const state = useSelector(state => ({
+                emojis: state.emojisRed.emojis,
+                loading: state.emojisRed.loading,
+                results: state.emojisRed.results
+        }))
+        const history = useHistory()
+
+        const renderAccEmojis = () => {
+                if (history.location.pathname === '/') {
+                        return state.results
+                }  else {
+                        return state.emojis
+                }
+        }
         
-        // const renderEmojis = emojiData.map((emoji, index) => {
-        //         if (!!emoji.variants) {
-        //                 return (
-        //                         <li key={index} style={{display: "flex"}}>
-        //                                 <div><EmojiChildComponent emoji={ emoji } /></div>
-        //                                 <ul>
-        //                                         {
-        //                                                 emoji.variants.map((variant, index) => {
-        //                                                         return ifEmoji(variant.character) ? <li key={ index }><EmojiChildComponent emoji={variant} /></li> : null
-        //                                                 })
-        //                                         }
-        //                                 </ul>
-        //                         </li>
-        //                 )
-        //         } else {
-        //                 return ifEmoji(emoji.character) ? <li key={index}>{emoji.character}</li> : null
-        //         }
-        // })
+        const renderEmojis = renderAccEmojis().map((emoji, index) => {
+                if (emoji.attributes.variants.length > 0) {
+                        return (
+                                <li key={index} style={{display: "flex"}} className="emoji">
+                                        <div className="parent_emoji">
+                                                <div className="emoji" onMouseEnter={null}><EmojiChildComponent emoji={ emoji } /> </div>
+                                                <div className="caret">
+                                                        <i className="fas fa-caret-right"></i>
+                                                </div>
+                                        </div>
+                                        {/* {
+                                                showVariants && (
+                                                        <ul className="variants_container" onMouseLeave={setShowVariants(false)}>
+                                                                <li>{<EmojiChildComponent emoji={emoji} />}</li>
+                                                                {
+                                                                        emoji.attributes.variants.map((variant, index) => {
+                                                                                return ifEmoji(variant.character) ? <li key={ index }><EmojiChildComponent emoji={variant} /></li> : null
+                                                                        })
+                                                                }
+                                                        </ul>
+                                                )
+                                        } */}
+                                </li>
+                        )
+                } else {
+                        return ifEmoji(emoji.attributes.character) ? <li key={index}>{emoji.attributes.character}</li> : null
+                }
+        })
+        
         return (
-                <ul>
-                        { null }
-                        {/* { renderEmojis } */}
-                </ul>
+                <>      
+                        {/* <p>{renderEmojis.length} results</p> */}
+                        {
+                                state.loading && (
+                                        <p>LOADING....</p>
+                                )
+                        }
+                        <ul className="emojis_container">
+                                { renderEmojis }
+                        </ul>
+                </>
         )
 }
 
