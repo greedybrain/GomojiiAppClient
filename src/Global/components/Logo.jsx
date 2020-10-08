af37aaf96e03909ae8257cf82499129cd002bfa0
greedybrain <greedybrain18@gmail.com>
2020-10-08 08:14:48 -0400

    created logo component

---
+++ src/Global/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+        return (
+                <div className="logo">
+                        <img src="images/mini-logo-white.png" alt=""/>
+                </div>
+        )
+}
+
+export default Logo
