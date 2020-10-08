0c4e2999e811dfd2b1afb7497a2aff92e1909591
greedybrain <greedybrain18@gmail.com>
2020-10-08 08:18:53 -0400

    rem logout & emojis cont/ add side nav/playground

--- src/Pages/HomePage/containers/HomeContainer.jsx
+++ src/Pages/HomePage/containers/HomeContainer.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import Logout from '../../../Global/components/Logout'
-import EmojisChildContainer from '../components/EmojisChildContainer'
+import Playground from '../components/Playground'
+import SideNavContainer from '../components/SideNavContainer'
+import '../../../Assets/homeContainer.css'
 
 const HomeContainer = () => {
         return (
                 <div className="home_page_container">
-                        <Logout />
-                        <EmojisChildContainer />
+                        <SideNavContainer />
+                        <Playground />
                 </div>
         )
 }
