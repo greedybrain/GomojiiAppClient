c1243b1b988111723b05c1354b84a967edd4ce72
greedybrain <greedybrain18@gmail.com>
2020-10-08 08:16:05 -0400

    updated nested structure of emoji json data

--- src/Pages/HomePage/components/EmojiChildComponent.jsx
+++ src/Pages/HomePage/components/EmojiChildComponent.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const EmojiChildComponent = ({ emoji }) => {
         return (
-                <>{ emoji.character }</>
+                <>
+                        { emoji.attributes.character }
+                </>
         )
 }
 
