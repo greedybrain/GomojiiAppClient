08ec9cbdcc9113718770fa893f930e33176b5e1c
greedybrain <greedybrain18@gmail.com>
2020-10-11 19:34:16 -0400

    specified imports

--- src/Store/middleware/emojisThunk.js
+++ src/Store/middleware/emojisThunk.js
@@ -1,17 +1,18 @@
+//!BUILT IN
+//!CUSTOM
 import {_get} from '../../Utils/config'
 import axios from 'axios'
-import { filterEmojis, loadEmojis, requestingEmojis } from '../emojis'
+import { loadEmojis, requestingEmojis } from '../emojis'
 
 const { baseUrl, emojis } = _get.endpoints
 
 export const loadEmojisThunk = () => {
-        return async dispatch => {
+        return async (dispatch, getState) => {
                 try {
                         dispatch(requestingEmojis())
                         const response = await axios(`${baseUrl}/${emojis}`)
                         const fetchedEmojis = response.data.emojis.data
                         dispatch(loadEmojis(fetchedEmojis))
-                        dispatch(filterEmojis(fetchedEmojis))
                 } catch(e) {
                         console.log(e)
                 }
