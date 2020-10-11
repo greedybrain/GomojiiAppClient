a02a8213001ade2fc664f4971ad9a94c5d55ec51
greedybrain <greedybrain18@gmail.com>
2020-10-11 19:33:09 -0400

    added userEmojis prop to state

--- src/Store/auth.js
+++ src/Store/auth.js
@@ -1,8 +1,9 @@
-import { act } from "react-dom/test-utils"
+// import { act } from "react-dom/test-utils"
 
 //! REDUCER
 const initialState = {
         user: {},
+        userEmojis: [],
         loggedIn: false,
         error: ''
 }
@@ -33,6 +34,16 @@ export default function authReducer(state = initialState, action) {
                                 error: action.payload.error,
                                 loggedIn: false,
                         }
+                case LOAD_USERS_EMOJIS:
+                        return {
+                                ...state,
+                                userEmojis: [action.payload.userEmojis]
+                        }
+                case SAVE_EMOJI:
+                        return {
+                                ...state,
+                                userEmojis:  [action.payload.emoji, ...state.userEmojis]
+                        }
                 case LOGOUT_USER:
                         return {
                                 ...state,
@@ -48,6 +59,8 @@ export default function authReducer(state = initialState, action) {
 const CHECK_USER_LOGGED_IN_STATUS = "CHECK_USER_LOGGED_IN_STATUS"
 const SIGNUP_USER = "SIGNUP_USER"
 const LOGIN_USER = "LOGIN_USER"
+const LOAD_USERS_EMOJIS = 'LOAD_USERS_EMOJIS'
+const SAVE_EMOJI = 'SAVE_EMOJI'
 const LOGOUT_USER = "LOGOUT_USER"
 const CATCH_FAILED_ERRORS = "CATCH_FAILED_ERRORS"
 
@@ -78,6 +91,20 @@ export const catchFailedErrors = error => ({
         type: CATCH_FAILED_ERRORS,
         payload: {
                 error
+        }
+})
+
+export const loadUsersEmojis = userEmojis => ({
+        type: LOAD_USERS_EMOJIS,
+        payload: {
+                userEmojis
+        }
+})
+
+export const saveEmoji = emoji => ({
+        type: SAVE_EMOJI,
+        payload: {
+                emoji
         }
 })
 
