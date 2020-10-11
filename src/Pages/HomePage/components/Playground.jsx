14cef42d4a4808ef397d3cb99feefd8f9a30bc0f
greedybrain <greedybrain18@gmail.com>
2020-10-11 19:31:12 -0400

    moved results div to header comp

--- src/Pages/HomePage/components/Playground.jsx
+++ src/Pages/HomePage/components/Playground.jsx
@@ -2,17 +2,28 @@ import React from 'react'
 import Header from './Header'
 import '../../../Assets/playground.css'
 import EmojisListComponent from './EmojisListComponent'
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { loadEmojisThunk } from '../../../Store/middleware/emojisThunk'
-import { loadEmojis } from '../../../Store/emojis'
+import { useSelector } from 'react-redux'
+// import { useEffect } from 'react'
+// import { useDispatch } from 'react-redux'
+// import { loadEmojisThunk } from '../../../Store/middleware/emojisThunk'
+// import { loadEmojis } from '../../../Store/emojis'
 
 const Playground = () => {
-        
-
+        const results = useSelector(state => state.emojisRed.results)  
         return (
                 <div className="playground_container">
+                        <div className="ad"></div>
                         <Header />
+                        {/* <div className="results">
+                                {
+                                        results.length === 1 
+                                        ?
+                                        `${results.length} result`
+                                        :
+                                        `${results.length} results`
+                                }
+                                
+                        </div> */}
                         <div className="emojis_and_ads">
                                 {<EmojisListComponent />}
                                 <div className="ads">
