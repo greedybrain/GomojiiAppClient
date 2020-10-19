327f67cd5cf115814e760812a92d7919602797f0
greedybrain <greedybrain18@gmail.com>
2020-10-19 16:39:12 -0400

    removed results

--- src/Pages/HomePage/components/Playground.jsx
+++ src/Pages/HomePage/components/Playground.jsx
@@ -2,32 +2,18 @@ import React from 'react'
 import Header from './Header'
 import '../../../Assets/playground.css'
 import EmojisListComponent from './EmojisListComponent'
-import { useSelector } from 'react-redux'
-// import { useEffect } from 'react'
-// import { useDispatch } from 'react-redux'
-// import { loadEmojisThunk } from '../../../Store/middleware/emojisThunk'
-// import { loadEmojis } from '../../../Store/emojis'
 
 const Playground = () => {
-        const results = useSelector(state => state.emojisRed.results)  
         return (
                 <div className="playground_container">
                         <div className="ad"></div>
                         <Header />
-                        {/* <div className="results">
-                                {
-                                        results.length === 1 
-                                        ?
-                                        `${results.length} result`
-                                        :
-                                        `${results.length} results`
-                                }
-                                
-                        </div> */}
                         <div className="emojis_and_ads">
                                 {<EmojisListComponent />}
                                 <div className="ads">
-
+                                        <div className="footer_ads_section">
+                                                <span aria-label="copyright emoji" role="img">©️</span> 2020 Gomojii
+                                        </div>
                                 </div>
                         </div>
                 </div>  
