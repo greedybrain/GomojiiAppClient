91a05204e3c2900c60e6f118089c4440b990a007
greedybrain <greedybrain18@gmail.com>
2020-10-19 16:39:30 -0400

    removed media query

--- src/Pages/HomePage/components/SearchForm.jsx
+++ src/Pages/HomePage/components/SearchForm.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 //! CUSTOM
-import { filterEmojis } from '../../../Store/emojis'
+import { searchEmojis } from '../../../Store/emojis'
 
-const SearchForm = ({ isAtLeast799 }) => {
+const SearchForm = () => {
         const [query, setQuery] = useState('')
         const emojis = useSelector(state => state.emojisRed.emojis)
         const dispatch = useDispatch()
@@ -16,32 +16,25 @@ const SearchForm = ({ isAtLeast799 }) => {
 
         const handleSubmit = event => {
                 event.preventDefault()
-                let results = emojis.filter(emoji => 
-                        emoji.attributes.slug.includes(query.toLowerCase()) ||
-                        emoji.attributes.slug === query.toLowerCase() ||
-                        emoji.attributes.unicodeName.includes(query.toLowerCase()) ||
-                        emoji.attributes.unicodeName === query.toLowerCase()
-                )
-                dispatch(filterEmojis(results))
+                dispatch(searchEmojis(emojis, query))
                 setQuery('')
         }
 
         return (
                 <form onSubmit={handleSubmit}>
-                        <div className="search_bar_area" style={isAtLeast799 ? { height: '40px' } : null }>
+                        <div className="search_bar_area">
                                 <div className="search_bar">
                                         <input 
                                                 type="search" 
                                                 name="search" 
-                                                placeholder="✍️   emoji keywords (i.e cat, dog)"
+                                                placeholder="✍️  Search emojis"
                                                 value={query}
                                                 onChange={handleChange}
                                                 className="mode_change"
-                                                style={isAtLeast799 ? { display: '1rem' } : null }
                                         />
                                 </div>
                                 <div className="submit_btn mode_change">
-                                        <button style={isAtLeast799 ? { display: '1rem' } : null }><span role="img" aria-label="Submit">🔍</span></button>
+                                        <button><span role="img" aria-label="Submit">🔍</span></button>
                                 </div>
                         </div>
                 </form>
