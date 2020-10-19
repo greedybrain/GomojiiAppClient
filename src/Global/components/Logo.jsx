1e7c95c2d4623ba53d27709b3fc18319c4c1545c
greedybrain <greedybrain18@gmail.com>
2020-10-19 16:37:15 -0400

    removed conditional

--- src/Global/components/Logo.jsx
+++ src/Global/components/Logo.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 const Logo = () => {
+        const history = useHistory()
         return (
+                history.location.pathname === '/'
+                ?
                 <div className="logo">
                         <img src="images/mini-logo-white.png" alt=""/>
+                </div>
+                :
+                <div className="logo">
+                        <img src="images/mini-logo-black.png" alt=""/>
                 </div>
         )
 }
