1eaadbe085b4cf22c5ce9f1f3da2a10e72bf2090
greedybrain <greedybrain18@gmail.com>
2020-10-19 16:38:01 -0400

    ...

--- src/Pages/HomePage/components/Emoji.jsx
+++ src/Pages/HomePage/components/Emoji.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Emoji = ({ emoji }) => {
         return (
                 <>
-                        { !!emoji.attributes ? emoji.attributes.character : emoji.character }
+                        { emoji.attributes ? emoji.attributes.character : emoji.character }
                 </>
         )
 }
