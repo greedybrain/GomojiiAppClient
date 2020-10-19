3910de7142ca3d403c4d5d3fef039a1afdeac21d
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-10-19 16:38:34 -0400

    removed media queries for js

--- src/Pages/HomePage/components/Header.jsx
+++ src/Pages/HomePage/components/Header.jsx
@@ -9,163 +9,62 @@ import SearchForm from './SearchForm'
 const Header = () => {
         const state = useSelector(state => ({
                 loggedIn: state.authRed.loggedIn,
-                results: state.emojisRed.results
+                results: state.emojisRed.results,
+                userEmojis: state.authRed.userEmojis
         }))
         const history = useHistory()
-        const IS_1330_ABOVE = useMediaQuery({minWidth: 1330})
-        const IS_BETWEEN_1200_1329 = useMediaQuery({ minWidth: 1200, maxWidth: 1329 })
-        const IS_BETWEEN_1000_1199 = useMediaQuery({ minWidth: 1000, maxWidth: 1199 })
-        const IS_BETWEEN_800_999 = useMediaQuery({ minWidth: 800, maxWidth: 999 })
-        const IS_AT_LEAST_799 = useMediaQuery({ maxWidth: 799 })
 
         return (
                 <header 
-                        className='mode_change' 
-                        style={IS_AT_LEAST_799 ? { padding: '0 15px', height: '8vh' } : null}
+                        className='mode_change'
                 >
-                        <SearchForm isAtLeast799={IS_AT_LEAST_799} />
+                        <SearchForm />
                         <ul>
+                                <li className="results">
+                                        {
+                                                `(${state.results.length})`
+                                        }
+                                </li>
+                                <li className="home" style={ history.location.pathname === '/' ? { borderBottom: '3px solid #333', borderRadius: '0 0 5px 5px' } : null }>
+                                        <NavLink to='/'>
+                                                <span role='img' aria-label='home'>🏠</span>
+                                        </NavLink>
+                                </li>
                                 {
-                                        IS_1330_ABOVE && (
-                                                <>
-                                                        <li className="results">
-                                                                {
-                                                                        state.results.length === 1 
-                                                                        ? `${state.results.length} Result`
-                                                                        : `${state.results.length} Results`
-                                                                }
-                                                        </li>
-                                                        <li className="home" style={ history.location.pathname === '/' ? { borderBottom: '3px solid #333', borderRadius: '0 0 5px 5px' } : null }>
-                                                                <NavLink to='/'>
-                                                                        <span role='img' aria-label='home'>🏠</span>
-                                                                </NavLink>
-                                                        </li>
-                                                        {
-                                                                state.loggedIn 
-                                                                ?
-                                                                <>
-                                                                        <li className="favs">
-                                                                                <span role='img' aria-label='fav'>❤️</span>
-                                                                        </li>
-                                                                        <Logout />
-                                                                </>
-                                                                :
-                                                                <>
-                                                                        <li className="login">
-                                                                                <NavLink to="/login">Login</NavLink>
-                                                                        </li>
-                                                                        <li className="signup">
-                                                                                <NavLink to="/signup">Signup</NavLink>
-                                                                        </li>
-                                                                </>
-                                                        }
-                                                        <li className="help">
-                                                                <span role='img' aria-label='help'>
-                                                                        🤷🏽‍♂️
-                                                                </span>
-                                                        </li>
-                                                </>
-                                        )
+                                        state.loggedIn 
+                                        ?
+                                        <>
+                                                <li className="favs">
+                                                        <span role='img' aria-label='fav'>❤️   </span>
+                                                        <span className="saves_count"> { state.userEmojis.length }</span>
+                                                </li>
+                                                <Logout />
+                                                <li className="help">
+                                                        <span role='img' aria-label='help'>
+                                                                🤷🏽‍♂️
+                                                        </span> 
+                                                </li>
+                                        </>
+                                        :
+                                        <>
+                                                <li className="login">
+                                                        <NavLink to="/login"><span role='img' aria-label="technologist" style={{ display: 'inline-block', marginRight: "5px" }}>🧑🏽‍💻</span>Login</NavLink>
+                                                </li>
+                                                <li className="signup">
+                                                        <NavLink to="/signup"><span role='img' aria-label="hands up" style={{ display: 'inline-block', marginRight: "5px" }}>🙋</span>Signup</NavLink>
+                                                </li>
+                                                <li className="help">
+                                                        <span role='img' aria-label='help'>
+                                                                🤷🏽‍♂️
+                                                        </span>
+                                                </li>
+                                        </>
                                 }
-                                {
-                                        //! SCREENS LOWER THAN 1330
-                                        IS_BETWEEN_1200_1329 && (
-                                                <>
-                                                        <li className="home" style={ history.location.pathname === '/' ? { borderBottom: '3px solid #333', borderRadius: '0 0 5px 5px' } : null }>
-                                                                <NavLink to='/'>
-                                                                        <span role='img' aria-label='home'>🏠</span>
-                                                                </NavLink>
-                                                        </li>
-                                                        {
-                                                                state.loggedIn 
-                                                                ?
-                                                                <>
-                                                                        <li className="favs">
-                                                                                <span role='img' aria-label='fav'>❤️</span>
-                                                                        </li>
-                                                                        <Logout />
-                                                                </>
-                                                                :
-                                                                <>
-                                                                        <li className="login">
-                                                                                <NavLink to="/login">Login</NavLink>
-                                                                        </li>
-                                                                        <li className="signup">
-                                                                                <NavLink to="/signup">Signup</NavLink>
-                                                                        </li>
-                                                                </>
-                                                        }
-                                                        <li className="help">
-                                                                <span role='img' aria-label='help'>
-                                                                        🤷🏽‍♂️
-                                                                </span>
-                                                        </li>
-                                                        <li className="menu_btn">
-                                                                <div className="line_1"></div>
-                                                                <div className="line_2"></div>
-                                                                <div className="line_3"></div>
-                                                        </li>
-                                                </>
-                                        )
-                                }
-                                {
-                                        IS_BETWEEN_1000_1199 && (
-                                                <>
-                                                       {
-                                                                state.loggedIn && (
-                                                                        <>
-                                                                                <li className="favs">
-                                                                                        <span role='img' aria-label='fav'>❤️</span>
-                                                                                </li>
-                                                                                <Logout />
-                                                                        </>
-                                                                )
-                                                        }
-                                                        <li className="help">
-                                                                <span role='img' aria-label='help'>
-                                                                        🤷🏽‍♂️
-                                                                </span>
-                                                        </li>
-                                                        <li className="menu_btn">
-                                                                <div className="line_1"></div>
-                                                                <div className="line_2"></div>
-                                                                <div className="line_3"></div>
-                                                        </li> 
-                                                </>
-                                        )
-                                }
-                                {
-                                        IS_BETWEEN_800_999 && (
-                                                <>
-                                                        {
-                                                                state.loggedIn && (
-                                                                        <>
-                                                                                <Logout />
-                                                                        </>
-                                                                )
-                                                        }
-                                                        <li className="help">
-                                                                <span role='img' aria-label='help'>
-                                                                        🤷🏽‍♂️
-                                                                </span>
-                                                        </li>
-                                                        <li className="menu_btn">
-                                                                <div className="line_1"></div>
-                                                                <div className="line_2"></div>
-                                                                <div className="line_3"></div>
-                                                        </li> 
-                                                </>
-                                        )
-                                }
-                                {
-                                        IS_AT_LEAST_799 && (
-                                                <li className="menu_btn">
-                                                        <div className="line_1"></div>
-                                                        <div className="line_2"></div>
-                                                        <div className="line_3"></div>
-                                                </li> 
-                                        )
-                                }
+                                <li className="menu_btn">
+                                        <div className="line_1"></div>
+                                        <div className="line_2"></div>
+                                        <div className="line_3"></div>
+                                </li>         
                         </ul>
                 </header>
         )
