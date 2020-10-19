4adb9f448a4290472e73aab1f8d8307b524bd908
greedybrain <greedybrain18@gmail.com>
2020-10-19 16:36:34 -0400

    ...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import Routes from './Routes/Routes';
 // import { loadEmojis } from './Store/emojis';
 import { checkLoggedInStatusThunk } from './Store/middleware/authThunk';
@@ -9,10 +10,11 @@ import { loadEmojisThunk } from './Store/middleware/emojisThunk';
 function App() {
 
   const dispatch = useDispatch()
-  
+  const history = useHistory()
+
   useEffect(() => {
-          dispatch(checkLoggedInStatusThunk())
-          // dispatch(loadEmojisThunk())
+          dispatch(checkLoggedInStatusThunk(history))
+          dispatch(loadEmojisThunk())
   })
 
   return (
