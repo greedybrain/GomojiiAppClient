cee731532ebd56a79dded2724d30b5d1aba0c225
greedybrain <greedybrain18@gmail.com>
2020-10-19 16:37:33 -0400

    ...

--- src/Global/components/Logout.jsx
+++ src/Global/components/Logout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 import { logoutUserThunk } from '../../Store/middleware/authThunk'
 
