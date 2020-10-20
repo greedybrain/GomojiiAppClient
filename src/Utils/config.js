401a026b943778da5a3c9d4804d8d4feb8231a31
greedybrain <greedybrain18@gmail.com>
2020-10-20 12:22:46 -0400

    ...

--- src/Utils/config.js
+++ src/Utils/config.js
@@ -1,7 +1,7 @@
 export const _get = {
         endpoints: {
-                baseUrl: "http://localhost:3001/api/v1",
-                // baseUrl: "https://gomojii.herokuapp.com/api/v1",
+                // baseUrl: "http://localhost:3001/api/v1",
+                baseUrl: "https://gomojii.herokuapp.com/api/v1",
                 loginCheck: 'login_check',
                 signup: 'signup',
                 login: 'login',
