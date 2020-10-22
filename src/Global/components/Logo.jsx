cbfbf2c8d130b731ca35cac41796982bd0f0f557
greedybrain <greedybrain18@gmail.com>
2020-10-22 11:23:44 -0400

    ...

--- src/Global/components/Logo.jsx
+++ src/Global/components/Logo.jsx
@@ -7,11 +7,11 @@ const Logo = () => {
                 history.location.pathname === '/'
                 ?
                 <div className="logo">
-                        <img src='/mini-logo-white.png' alt="white logo"/>
+                        <img src='/images/mini-logo-white.png' alt="white logo"/>
                 </div>
                 :
                 <div className="logo">
-                        <img src='/mini-logo-black.png' alt="black logo"/>
+                        <img src='/images/mini-logo-black.png' alt="black logo"/>
                 </div>
         )
 }
