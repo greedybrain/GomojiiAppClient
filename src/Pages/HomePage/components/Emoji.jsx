f8abfc7f5f5eda20ead5a8705b4f2abaf4b7b69f
greedybrain <greedybrain18@gmail.com>
2020-10-22 20:19:58 -0400

    ...

--- src/Pages/HomePage/components/Emoji.jsx
+++ src/Pages/HomePage/components/Emoji.jsx
@@ -6,7 +6,7 @@ const Emoji = ({ emoji }) => {
         const userData = useSelector(state => state.authRed.user)
         const handleCopyEmoji = ()  => userData.user.id ? alert('Emoji saved and copied') : alert('Emoji copied (Login or Signup to save one')
         return (
-                <li title={emoji.attributes.slug}>
+                <li>
                         <CopyToClipboard text={ emoji.attributes ? emoji.attributes.character : emoji.character }>
                                 <span onDoubleClick={handleCopyEmoji} data-id={emoji.id} style={{ display: "inline-block" }}>
                                         { emoji.attributes ? emoji.attributes.character : emoji.character }
