a95558944b140142a1dedbc8e35e87c0eed7c82a
greedybrain <greedybrain18@gmail.com>
2020-10-24 10:59:17 -0400

    ...

--- src/Pages/HomePage/components/Playground.jsx
+++ src/Pages/HomePage/components/Playground.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import Header from './Header'
 import '../../../Assets/playground.css'
 import EmojisListComponent from './EmojisListComponent'
-// import sampleAd1 from '../../../Images/sample-ad-1.jpg'
-// import sampleAd2 from '../../../Images/sample-ad-2.png'
 import MenuDrawer from '../../../Global/components/MenuDrawer'
 
 const Playground = () => {
@@ -16,6 +14,18 @@ const Playground = () => {
                                 <EmojisListComponent />
                                 <div className="ads">
                                         <ul className="footer_ads_section">
+                                                <li className="ad_1">
+                                                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                                                        <ins class="adsbygoogle"
+                                                        style={{display: 'block'}}
+                                                        data-ad-client="ca-pub-7186715356628967"
+                                                        data-ad-slot="6294394158"
+                                                        data-ad-format="auto"
+                                                        data-full-width-responsive="true"></ins>
+                                                        <script>
+                                                                (adsbygoogle = window.adsbygoogle || []).push({});
+                                                        </script>
+                                                </li>
                                                 <li className="copyright_footer">
                                                         <span aria-label="copyright emoji" role="img">©️</span> 2020 Gomojii
                                                 </li>
